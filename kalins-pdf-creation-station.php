d6de13959b891fc8e30299c6cdf211bc773740ee
kalinbooks <kalinbooks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-08 18:59:25 +0000

    No real changes. added a commented excerpt function that I may use later
    git-svn-id: https://plugins.svn.wordpress.org/kalins-pdf-creation-station/trunk@342592 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kalins-pdf-creation-station.php
+++ kalins-pdf-creation-station.php
@@ -754,6 +754,37 @@ function kalins_pdf_page_shortcode_replace($str, $page){//replace all passed in 
 	return $str;
 }
 
+
+/*
+
+//try using this function
+function my_excerpt($text, $excerpt)
+{
+    if ($excerpt) return $excerpt;
+
+    $text = strip_shortcodes( $text );
+
+    $text = apply_filters('the_content', $text);
+    $text = str_replace(']]>', ']]&gt;', $text);
+    $text = strip_tags($text);
+    $excerpt_length = apply_filters('excerpt_length', 55);
+    $excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
+    $words = preg_split("/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY);
+    if ( count($words) > $excerpt_length ) {
+            array_pop($words);
+            $text = implode(' ', $words);
+            $text = $text . $excerpt_more;
+    } else {
+            $text = implode(' ', $words);
+    }
+
+    return apply_filters('wp_trim_excerpt', $text, $raw_excerpt);
+}
+
+
+*/
+
+
 class KalinsPDF_ExcerptCallback{
 	function postExcerptCallback($matches){
 		if(isset($matches[2])){
